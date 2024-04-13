7c49c66bdf583e98a20db2d6636a108380729daa
Adrian Bernabeu <mentan017@gmail.com>
2024-04-13 21:36:33 +0200

    Made the menu functional for mobile devices

---
+++ Home/script.js
@@ -0,0 +1,6 @@
+document.getElementById("burger-menu").addEventListener("click", function(e){
+    document.getElementById("phone-navigation-menu").classList.toggle("active");
+});
+document.getElementById("exit-cross").addEventListener("click", function(e){
+    document.getElementById("phone-navigation-menu").classList.toggle("active");
+});
